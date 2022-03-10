419f62175542bdd43f674ba1746b6b360dfa3c08
Alireza <alirezark@gmail.com>
2022-03-10 14:14:31 +0330

    feat: child dashboard

--- src/layouts/parent-dashboard-layout.tsx
+++ src/layouts/parent-dashboard-layout.tsx
@@ -4,10 +4,11 @@ import LogoImage from '../public/images/logo.png';
 import DashboardIcon from 'layouts/icons/dashboard';
 import {ThemeProvider} from '@mui/material';
 import {theme, RTL} from './muiBase';
+import BaseLayout from './baseLayout';
 
 export type ParentDashboardLayoutProps = {
   children: React.ReactNode;
-  SideComponent: React.ReactNode;
+  SideComponent?: React.ReactNode;
 };
 
 const styles = {
@@ -34,10 +35,11 @@ const NavBar = () => {
         textAlign: 'center',
         p: 2
       }}>
-        <Box sx={{ maxWidth: 210, p: 2, margin: '0 auto' }}><Image src={LogoImage} alt="logo"/></Box>
-        <Box sx={{ mt: 5}}>
+        <Box sx={{maxWidth: 210, p: 2, margin: '0 auto'}}><Image src={LogoImage} alt="logo"/></Box>
+        <Box sx={{mt: 5}}>
           <Button sx={styles.navButton} color="primary" startIcon={<DashboardIcon/>}>داشبورد</Button>
-          <Button sx={{...styles.navButton, ...styles.activeNavButton}} variant="contained" startIcon={<DashboardIcon/>}>داشبورد</Button>
+          <Button sx={{...styles.navButton, ...styles.activeNavButton}} variant="contained"
+                  startIcon={<DashboardIcon/>}>داشبورد</Button>
           <Button sx={styles.navButton} color="primary" startIcon={<DashboardIcon/>}>داشبورد</Button>
           <Button sx={styles.navButton} color="primary" startIcon={<DashboardIcon/>}>داشبورد</Button>
         </Box>
@@ -46,21 +48,19 @@ const NavBar = () => {
 }
 
 export default function ParentDashboardLayout({children, SideComponent}: ParentDashboardLayoutProps) {
-  return <ThemeProvider theme={theme}>
-    <RTL>
-      <Grid container spacing={2}>
-        <Grid item sx={{width: 300}}>
-          <NavBar/>
-        </Grid>
-        <Grid item xs>
-          <Box sx={{ borderRadius: 8, p: 2, mt: 2, maxWidth: '98%', background: '#F5F9F8', minHeight: '90vh'}}>
-            {children}
-          </Box>
-        </Grid>
-        {!!SideComponent && <Grid item sx={{width: { lg: 300, md: 250 }}}>
-          {SideComponent}
-        </Grid>}
+  return <BaseLayout>
+    <Grid container spacing={2}>
+      <Grid item sx={{width: 300}}>
+        <NavBar/>
       </Grid>
-    </RTL>
-  </ThemeProvider>;
+      <Grid item xs>
+        <Box sx={{borderRadius: 8, p: 2, mt: 2, maxWidth: '98%', background: '#F5F9F8', minHeight: '90vh'}}>
+          {children}
+        </Box>
+      </Grid>
+      {!!SideComponent && <Grid item sx={{width: {xl: 300, xs: 250}}}>
+        {SideComponent}
+      </Grid>}
+    </Grid>
+  </BaseLayout>;
 }
