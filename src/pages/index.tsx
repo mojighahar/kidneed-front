ace94be2ab19f529ad5bbaae4e928b35cebaa831
mohammad-ILA <mghazim93@gmail.com>
2022-03-10 12:06:55 +0330

    logo and menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
 import { openGuard } from "@kidneed/utils";
+import styles from 'landing/styles/index.module.css'
+import Image from 'next/image'
+import logo from 'landing/media/images/logo.png'
 
 export default function Home() {
-  return <div>home</div>;
+  return <div className={styles.landing}>
+    <div className={styles.menubar}>
+      <div className={styles.menu}>
+        <div className={styles.logo}>
+          <Image src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  </div>;
 }
 
 Home.guard = openGuard;
