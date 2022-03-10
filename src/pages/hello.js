7aa6af5ac7a63d872aeafa8a610299645ba306c7
mohammad-ILA <mghazim93@gmail.com>
2022-03-10 12:20:41 +0330

    header and menu

---
+++ src/pages/hello.js
@@ -0,0 +1,8 @@
+
+
+const Hello = () => {
+    return (<div></div>)
+}
+export default Hello;
+
+Hello.guard = () => true;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { openGuard } from "@kidneed/utils";
 import styles from 'landing/styles/index.module.css'
 import Image from 'next/image'
 import logo from 'landing/media/images/logo.png'
+import Link from "next/link";
 
 export default function Home() {
   return <div className={styles.landing}>
@@ -10,9 +11,46 @@ export default function Home() {
         <div className={styles.logo}>
           <Image src={logo} alt="logo" />
         </div>
+        <div className={styles.menuItems}>
+          <div>
+            <Link href="/">صفحه اصلی</Link>
+          </div>
+          <div>
+            <Link href="/">درباره ما</Link>
+          </div>
+          <div>
+            <Link href="/">تماس با ما</Link>
+          </div>
+          <div>
+            <Link href="/">بلاگ</Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.login}>
+        <Link href="/login">
+          <button type="button" className={styles.loginBtn}>ورود</button>
+        </Link>
+      </div>
+    </div>
+    <div className={styles.header}>
+      <div className={styles.title}>
+        <h1>با برنامه دقیق، راه پیشرفت کودکانمان را خود بسازیم </h1>
+      </div>
+      <div className={styles.desc}>
+        <h3>
+          پلنت، تعیین برنامه هوشمند اوقات فراغت و درسی کودک شما
+        </h3>
+      </div>
+      <div className={styles.buttons}>
+        <Link href="" >
+          <button type="button" className={styles.btnStartTrip}>سفر را شروع کنید</button>
+        </Link>
+        <Link href="">
+          <button type="button" className={styles.knowMoreBtn}>بیشتر بدانید</button>
+        </Link>
       </div>
     </div>
   </div>;
 }
 
-Home.guard = openGuard;
+Home.guard = () => true;
